410a29edac619f6dc0071fb52ef7a8ff64da24de
whyest <whyest@gmail.com>
2023-04-26 17:16:12 +0300

    added russian interface

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
-import React, {useState, useEffect, useRef} from "react"
-import useWordGame from "./hooks/useWordGame"
+import React, { useState, useEffect, useRef } from 'react'
+import useWordGame from './hooks/useWordGame'
 
 function App() {
-    const {
-        textBoxRef, 
-        handleChange, 
-        text, 
-        isTimeRunning, 
-        timeRemaining, 
-        startGame, 
-        wordCount
-    } = useWordGame(60)
-    
-    return (
-        <div>
-            <h1>How fast do you type?</h1>
-            <textarea
-                ref={textBoxRef}
-                onChange={handleChange}
-                value={text}
-                disabled={!isTimeRunning}
-            />
-            <h4>Time remaining: {timeRemaining}</h4>
-            <button 
-                onClick={startGame}
-                disabled={isTimeRunning}
-            >
-                Start
-            </button>
-            <h1>Word count: {wordCount}</h1>
-        </div>
-    )
+  const {
+    textBoxRef,
+    handleChange,
+    text,
+    isTimeRunning,
+    timeRemaining,
+    startGame,
+    wordCount,
+  } = useWordGame(60)
+
+  return (
+    <div>
+      <h1>Сколько слов в минуту ты печатаешь?</h1>
+      <textarea
+        ref={textBoxRef}
+        onChange={handleChange}
+        value={text}
+        disabled={!isTimeRunning}
+      />
+      <h4>Время: {timeRemaining}</h4>
+      <button onClick={startGame} disabled={isTimeRunning}>
+        Старт
+      </button>
+      <h1>Количество слов: {wordCount}</h1>
+    </div>
+  )
 }
 
 export default App
